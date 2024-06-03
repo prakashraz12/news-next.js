43d7cef348355f4590fb59b5a45f953f3f81f63e
Prakash Shrestha <rzprakash16@gmail.com>
2024-06-03 21:07:26 +0545

    bug-fix

--- hook/useDeviceType.tsx
+++ hook/useDeviceType.tsx
@@ -1,20 +1,27 @@
 "use client";
 import { useState, useEffect } from 'react';
 
-export const useDeviceType = (): boolean => {
-  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
 
-  const updateIsMobile = (): void => {
-    setIsMobile(window.innerWidth <= 768);
-  };
+export const useDeviceType = (): boolean => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useEffect(() => {
-    window.addEventListener('resize', updateIsMobile);
+    // Check if window is defined to ensure it's executed on the client-side
+    if (typeof window !== 'undefined') {
+      setIsMobile(window.innerWidth <= 768);
+    }
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
     return () => {
-      window.removeEventListener('resize', updateIsMobile);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return isMobile;
 };
-
